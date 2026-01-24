abb68e75471a67f08d79dfb5536ffa8739826470
Enzo <katiagauberti@gmail.com>
2026-01-24 16:45:35 +0100

    Ajout des RI pdf

--- backend/src/routes/maintenances.routes.js
+++ backend/src/routes/maintenances.routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const maintenancesController = require("../controllers/maintenancesController.js");
 
-// IMPORTANT: Routes spécifiques AVANT les routes génériques
-router.get("/", maintenancesController.getAllMaintenances);
+// Routes spécifiques AVANT /:id
+router.get("/:id/html", maintenancesController.generateMaintenanceHTML);
+router.get("/:id/pdf", maintenancesController.generateMaintenancePDF);
 router.get("/AllMaintenancesBySiteID/:id", maintenancesController.getAllMaintenancesBySiteID);
+
+router.get("/", maintenancesController.getAllMaintenances);
 router.get("/:id", maintenancesController.getMaintenanceById);
 router.post("/", maintenancesController.createMaintenance);
 router.put("/:id", maintenancesController.updateMaintenance);
